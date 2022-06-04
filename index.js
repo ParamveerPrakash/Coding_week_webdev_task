7b4455b10f7873930715266fc64e2f404c69856f
ParamveerPrakash <98461297+ParamveerPrakash@users.noreply.github.com>
2022-06-04 22:01:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const productsList = document.getElementById('productsList');
-const searchBar = document.getElementById('searchBar');
+const productsList = document.getElementById("productsList");
+const searchBar = document.getElementById("searchBar");
 let listedProducts = [];
-searchBar.addEventListener('keyup', (e) => {
-    const searchString = e.target.value.toLowerCase();
-    console.log(searchString);
-    const data=listedProducts.products;
-    const filteredProducts = data.filter((product) => {
-        return (
-            product.title.toLowerCase().includes(searchString) ||product.description.toLowerCase().includes(searchString)
-        );
-    });
-    displayProducts(filteredProducts);
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value.toLowerCase();
+  console.log(searchString);
+  const data = listedProducts.products;
+  const filteredProducts = data.filter((product) => {
+    return (
+      product.title.toLowerCase().includes(searchString) ||
+      product.description.toLowerCase().includes(searchString)
+    );
+  });
+  displayProducts(filteredProducts);
 });
 const loadProducts = async () => {
-    try {
-        const res = await fetch("product_dummy_data.txt");
-        listedProducts = await res.json();
-        console.log(res);
-        console.log(listedProducts);
-        const data=listedProducts.products;
-        displayProducts(data);
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const res = await fetch("product_dummy_data.txt");
+    listedProducts = await res.json();
+    //console.log(res);
+    //console.log(listedProducts);
+    const data = listedProducts.products;
+    console.log(data);
+    displayProducts(data);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const displayProducts = (data) => {
-    const htmlString = data
-        .map((product) => {
-            return `
+  const htmlString = data
+    .map((product) => {
+      return `
             <li class="product">
                 <h2 class="title">${product.title}</h2>
                 <p class="desc">${product.description}</p>
                 <img src="${product.images[0]}" class="img"></img>
             </li>
         `;
-        })
-        .join('');
-    productsList.innerHTML = htmlString;
+    })
+    .join("");
+  productsList.innerHTML = htmlString;
 };
 
 loadProducts();
